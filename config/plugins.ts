0530002c5ae53762d6aedf5541ec2934de37a40a
olek <oleksandrp990@gmail.com>
2024-12-26 06:07:35 +0000

    add cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
